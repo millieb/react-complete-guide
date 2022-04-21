ac08bd33f0ed62336a5b3a6cca538c3d6c6835cd
millieb <mildred.brito@outlook.com>
2022-04-20 21:35:10 -0700

    Adding day 4/100 days of code

--- 2-InheritanceBased-ObjectModeling/constructorFunctions.js
+++ 2-InheritanceBased-ObjectModeling/constructorFunctions.js
@@ -25,4 +25,30 @@ const tx = new HashTransaction("mildred@email.com", "alex@email.com");
 tx.calculateHash(); // returns hash value
 tx.sender; // 'mildred@email.com'
 
-// left of: 2.2.2 Sharing Properties by Using Constructors and Prototypes
+/* left of: 2.2.2 Sharing Properties by Using Constructors and Prototypes */
+
+// Adding calculateHash to HasTransactions to make it shareable among all HashTransaction instances
+HashTransaction.prototype.calculateHash = function calculateHash() {
+  const data = [this.sender, this.recipient].join("");
+  let hash = 0,
+    i = 0;
+  while (i < data.length) {
+    hash = ((hash << 5) - hash + data.charCodeAt(i++)) << 0;
+  }
+  return hash * 2;
+};
+
+// Adding a new method to Transaction.prototype to make function shareable among all objects
+Transaction.prototype.displayTransaction = function displayTransaction() {
+  return "Transaction from ${this.sender} to ${this.recipient}";
+}; // To avoid any type errors we need to configure the prototype chain
+
+/* 2.6: Configuring the Prototype Chaing using the Constructor Functions Pattern */
+
+// Links prototypes for the lookup mechanism to work in case you need to resolve properties
+// from Transaction.prototype
+HashTransaction.prototype = Object.create(Transaction.prototype);
+
+// Fixes or sets the constructor value
+// Without this line, tx would be a transaction object or constructed from transaction
+HashTransaction.prototype.constructor = HashTransaction;
---
+++ 3-LinkedCompositionalObjectModels/TypesOfObjectLinks.js
@@ -0,0 +1,3 @@
+/* 3.1 - Types of Object Links */
+
+// Left off here
