ad11f1a014bef408abb8194e4815b3fd4a402ba2
millie <mily.brit@gmail.com>
2022-06-01 20:36:24 -0700

    Filter is now working.

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -5,17 +5,21 @@ import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2022');
+    const [filteredYear, setFilteredYear] = useState('2021');
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {props.items.map(expense =>
+                {filteredExpenses.map(expense =>
                     <ExpenseItem
                         key={expense.id}
                         title={expense.title}
